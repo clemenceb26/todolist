6f86d21e3bf448b16b871f3a4a4e3d9455838979
BODEREAU <Cl.BODEREAU@formation-cci.fr>
2023-04-12 15:54:06 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link rel="stylesheet" href="styles/styles.css">
+    <title>Evaluation php</title>
+</head>
+<body>
+    <nav>
+        <img src="images/logo.png" alt="logo">
+        <div class="button">
+            <button></button>
+        </div>
+    </nav>
+    
+    <main>
+        <h1>Ma toDolist</h1>
+        
+    </main>
+
+   
+</body>
+</html>
